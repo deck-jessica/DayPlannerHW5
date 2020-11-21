cdcb9d834b1164a2897e3082725907b3267b1303
Jessica D <deck.jessica@gmail.com>
2020-11-21 16:44:39 -0500

    added function for color coding and ids on each HTML input item, color code not working yet

--- script.js
+++ script.js
@@ -2,9 +2,29 @@
 var plannerDay = moment();
 $('#currentDay').text(plannerDay.format("dddd, MMMM Do YYYY, h:mm a"));
 
+// function to change color based on past, present and future
+var currentTime = parseInt(moment().format('H'));
+$('.time-block').each(function () {
+    var hourSlot = parseInt($(this).attr('id'));
+    if (hourSlot > currentTime) {
+        $(this).addClass("future");
+    } else if (hourSlot < currentTime ) {
+        $(this).addClass("past");
+    } else if (hourSlot === currentTime) {
+        $(this).addClass("present");
+    };
+})
+
+
+
 // save task to hour it has been assigned to
-var hourTask = $('input[name=')
+var saveTask = $()
+var hourTaskEl = $('input[name="hour-input"]');
 function handleSaveTask (event) {
     event.preventDefault();
-
+    var task = hourTaskEl.val();
 }
+
+$('button').on('click', function() {
+
+})
